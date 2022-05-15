d24443da8879413888c011056f609aa84fc92494
WeichuanCai <73470479+WeichuanCai@users.noreply.github.com>
2022-05-15 19:32:05 -0400

    update

---
+++ app/main/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Length, DataRequired
+
+
+class TodoForm(FlaskForm):
+    todo = StringField("Enter your todo", validators=[DataRequired(), Length(1, 128)])
+    submit = SubmitField("Submit")
+
+
+class TodoListForm(FlaskForm):
+    title = StringField(
+        "Enter your todolist title", validators=[DataRequired(), Length(1, 128)]
+    )
+    submit = SubmitField("Submit")
